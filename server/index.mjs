47f4df555b596c74c803c57aa99f02df6c8d2385
Last_Stardust <andreaorso22@gmail.com>
2026-03-23 17:54:47 +0100

    Server
    Aggiunta del server

---
+++ server/index.mjs
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+// Serve i file statici dalla cartella client
+app.use(express.static(path.join(__dirname, '..', 'client')));
+
+// Route principale
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+});
+
+
+
+
+
+    app.listen(PORT, () => {
+    console.log(`Server avviato su http://localhost:${PORT}`);
+}); 
